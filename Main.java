9bb4561f3a05f9fa420cf3567e450ccb900061ef
MrColarex <mertkara256@gmail.com>
2024-04-27 19:27:39 +0300

    Main
    Starting main

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
-import java.util.*
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+public class Main {
+    public static void main(String[] args) {
+        // Read input from the text file
+        ArrayList<TaskType> taskTypes = new ArrayList<>();
+        ArrayList<JobType> jobTypes = new ArrayList<>();
+        ArrayList<Station> stations = new ArrayList<>();
+
         
-    }
-}
+    
