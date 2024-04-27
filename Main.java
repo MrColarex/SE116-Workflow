ca21ff03dee9f55aea8212a8d792a1aea20e7b93
MrColarex <mertkara256@gmail.com>
2024-04-27 21:22:58 +0300

    Checking command prompt

--- Main.java
+++ Main.java
@@ -7,10 +7,30 @@ import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
-        // Read input from the text file
-        ArrayList<TaskType> taskTypes = new ArrayList<>();
-        ArrayList<JobType> jobTypes = new ArrayList<>();
-        ArrayList<Station> stations = new ArrayList<>();
-
         
+        //This part makes sure user has entered workflow and job file names so it can read and write in them.
+        //Code will print error message if either file does not exist or it is inaccasible.
+
+        if (args.length != 2) {
+            System.out.println("Usage: java Main <workflow_file> <job_file>");
+            return;
+        }
+        
+        String workflowFilePath = args[0];
+        String jobFilePath = args[1];
+        
+        File workflowFile = new File(workflowFilePath);
+        if (!workflowFile.exists() || !workflowFile.canRead()) {
+            System.out.println("Error: Workflow file does not exist or is not accessible.");
+            return;
+        }
+        
+        File jobFile = new File(jobFilePath);
+        if (!jobFile.exists() || !jobFile.canRead()) {
+            System.out.println("Error: Job file does not exist or is not accessible.");
+            return;
+        }
+        
+    }
+}
     
